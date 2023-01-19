78b5fe39ec508ffb898fa8acc5f1917b062a00a4
Tu Nguyen <tu.t.nguyen93@gmail.com>
2023-01-18 19:36:37 -0500

    remove my-app

--- my-app/src/types/user.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    user_id?: number | string
-    first_name?: string
-    last_name?: string
-    display_name: string
-    profile_photo_url?: string
-    email?: string
-}
